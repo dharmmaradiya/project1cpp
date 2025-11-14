085cc877ca3f7ef748ab70d2a92915c564c713e9
dharmmaradiya <dharmmaradiya1906@gmail.com>
2025-11-14 12:03:46 +0530

    Add files via upload

---
+++ tcc.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+class timeconveter{
+    public:
+    int totalsecond;
+    int hour;
+    int minutes;
+    int remindersecond;
+    int second ;
+    void showdetails(){
+        cout<<hour<<":"<<minutes<<":"<<second<<endl;
+        
+    }
+};
+int main() {
+  timeconveter t1;
+  cout<<"enter the second:"<<endl;
+  cin>> t1.totalsecond;
+  t1.hour=t1.totalsecond/3600; 
+   t1.remindersecond=t1.totalsecond%3600; 
+    t1.minutes =t1.remindersecond/60; 
+    t1.second =t1.remindersecond%60; 
+  t1.showdetails();
+    
+    return 0;
+}
